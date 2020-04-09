0adb383c0fd0efeb2f12b48eff6e8a477c9fe8f6
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2020-04-09 22:44:51 +0200

    Add themes

---
+++ lib/theme/themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme() {
+  return ThemeData(brightness: Brightness.light, primaryColor: Colors.blue);
+}
+
+ThemeData darkTheme() {
+  return ThemeData(brightness: Brightness.dark, primaryColor: Colors.blue);
+}
