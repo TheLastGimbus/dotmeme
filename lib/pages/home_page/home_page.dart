1030b31d8095382e39fc521930823c2c782a9d3d
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2020-04-10 18:57:58 +0200

    Add some memes and basic grid view

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -4,12 +4,30 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var memesPaths = [
+      'assets/example_memes/t-regex.jpg',
+      'assets/example_memes/fixing-bugs.jpg',
+      'assets/example_memes/c-vs-rust.jpg',
+      'assets/example_memes/github-build.png',
+      'assets/example_memes/elon-distance.png',
+      'assets/example_memes/esk8.jpg',
+      'assets/example_memes/milk.jpg',
+      'assets/example_memes/fire.jpg',
+      'assets/example_memes/high-priority.jpg',
+      'assets/example_memes/duck.jpg',
+      'assets/example_memes/china.png',
+    ];
+
     return Scaffold(
       appBar: AppBar(title: Text('Dotmeme')),
-      body: Wrap(
-        children: <Widget>[
+      body: GridView.builder(
+        itemCount: memesPaths.length,
+        gridDelegate:
+        SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
 
-        ],
+        itemBuilder: (context, index) {
+          return Image.asset(memesPaths[index], fit: BoxFit.cover,);
+        },
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Search',
