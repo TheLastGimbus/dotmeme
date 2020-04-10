083fa0c0546db8e38993e57c4182b0359123afff
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2020-04-11 00:07:25 +0200

    Add swiping_page

---
+++ lib/pages/swiping_page/swiping_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SwipingPage extends StatefulWidget {
+  SwipingPage({@required this.imagesList, this.startIndex});
+
+  final List<String> imagesList;
+  final startIndex;
+
+  @override
+  State<StatefulWidget> createState() => _SwipingPageState(
+        imagesList: imagesList,
+        startIndex: startIndex,
+      );
+}
+
+class _SwipingPageState extends State<SwipingPage> {
+  // TODO: Change this to get memes from Provider or something
+  _SwipingPageState({@required this.imagesList, this.startIndex = 0});
+
+  List<String> imagesList;
+  final int startIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    final _controller = PageController(initialPage: startIndex);
+    return Scaffold(
+      body: PageView.builder(
+        controller: _controller,
+        itemBuilder: (context, index) {
+          return Image.asset(imagesList[index]);
+        },
+      ),
+    );
+  }
+}
