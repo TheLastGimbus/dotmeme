13728a546b64fab2890cf7b4c9bcd1e3feccdb53
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2020-04-11 20:52:02 +0200

    Update theme

--- lib/theme/themes.dart
+++ lib/theme/themes.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme() {
-  return ThemeData(brightness: Brightness.light, primaryColor: Colors.blue);
+  return ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    primaryColorLight: Colors.cyanAccent,
+    primaryColorDark: Colors.indigo,
+    accentColor: Colors.orange,
+  );
 }
 
 ThemeData darkTheme() {
-  return ThemeData(brightness: Brightness.dark, primaryColor: Colors.blue);
+  return ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    primaryColorLight: Colors.cyanAccent,
+    primaryColorDark: Colors.indigo,
+    accentColor: Colors.orange,
+  );
 }
