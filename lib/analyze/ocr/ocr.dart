24063984c4eb638e4e255ef5edc76d9952ed8292
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-04-16 15:57:30 +0200

    Basic class in case we need to change OCR provider later

---
+++ lib/analyze/ocr/ocr.dart
@@ -0,0 +1,11 @@
+import 'dart:core';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:tesseract_ocr/tesseract_ocr.dart';
+
+class Ocr {
+  static Future<String> getText(
+      {@required String imagePath, String language = 'eng'}) async {
+    return TesseractOcr.extractText(imagePath, language: language);
+  }
+}
