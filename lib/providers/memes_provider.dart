1450d970812f74bcdab650f5d36c13fb3a82a0a6
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-05-01 22:26:55 +0200

    Add stuff to MemesProvider - sync folders

--- lib/providers/memes_provider.dart
+++ lib/providers/memes_provider.dart
@@ -1,10 +1,50 @@
 // This provider keeps sync of all memes list
+import 'dart:developer';
+
+import 'package:dotmeme/database/memebase.dart';
 import 'package:flutter/widgets.dart';
+import 'package:moor_ffi/database.dart';
 import 'package:photo_manager/photo_manager.dart';
 
+/// This is the way you should access memes.
+/// Get all of them, search them, get only from certain folder.
+/// Also scan and sync them etc
+///
+/// This provider carries database object, and should be only one that has it
+/// Maybe in future, I will divide this to different provider with one top
+/// database provider.
 class MemesProvider with ChangeNotifier {
+  final db = Memebase();
+
   Future<List<AssetEntity>> get getAllMemes async {
-    var folders = await PhotoManager.getAssetPathList(type: RequestType.image);
-    return folders[0].assetList;
+    await syncFolders();
+    var dbMemes = await db.getAllMemes;
+    List<AssetEntity> memes = [];
+    for(var meme in dbMemes) memes.add(await AssetEntity.fromId(meme.id));
+    return memes;
+  }
+
+  Future syncFolders() async {
+    var folders = await PhotoManager.getAssetPathList(
+      hasAll: false,
+      type: RequestType.image,
+    );
+    for (AssetPathEntity fol in folders) {
+      try{
+        db.createFolder(FoldersCompanion.insert(
+          id: fol.id,
+          scanningEnabled: false,
+        ), ignoreFail: true);
+      } on SqliteException catch(e) {
+        // Already exists
+      }
+    }
+    for(Folder fol in await db.getAllFolders){
+      if(!folders.map((f) => f.id).contains(fol.id)){
+        db.deleteFolder(fol.createCompanion(false));
+      }
+    }
+
+    log("Folders: ${await db.getAllFolders}");
   }
 }
