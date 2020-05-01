f49398c9525bd222cc2e97e2b70142575f03aae1
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-05-02 00:15:41 +0200

    Finish this

--- lib/pages/settings_page/folders_settings_page.dart
+++ lib/pages/settings_page/folders_settings_page.dart
@@ -1,21 +1,41 @@
 import 'package:dotmeme/database/memebase.dart';
 import 'package:dotmeme/providers/memes_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 import 'package:provider/provider.dart';
 
-Widget _foldersListView(List<Folder> folders) => ListView(
-      children: folders
-          .map((folder) => SwitchListTile(
-                value: folder.scanningEnabled,
-                // This needs to be enough until photo_manager creator will fix
-                // TODO: Change this to get folder name
-                title: Text(folder.id),
-                // TODO: onChanged
-              ))
-          .toList(),
-    );
-
 class FoldersSettingsPage extends StatelessWidget {
+  // Workaround for now :/
+  // This needs to be enough until photo_manager creator will fix
+  Future<Map<String, String>> _loadNames() async {
+    var assFolders = await PhotoManager.getAssetPathList(
+      hasAll: false,
+      type: RequestType.image,
+    );
+    var names = Map<String, String>();
+    for (var ass in assFolders) {
+      names[ass.id] = ass.name;
+    }
+    return names;
+  }
+
+  Widget _foldersListView(MemesProvider memesProvider, List<Folder> folders) =>
+      FutureBuilder(
+        future: _loadNames(),
+        builder: (context, snapshot) => ListView(
+          children: folders
+              .map((folder) => SwitchListTile(
+                    value: folder.scanningEnabled,
+                    title: Text(
+                        snapshot.hasData ? snapshot.data[folder.id] : '...'),
+                    onChanged: (enabled) {
+                      memesProvider.setFolderSyncEnabled(folder, enabled);
+                    },
+                  ))
+              .toList(),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     var memesProvider = Provider.of<MemesProvider>(context);
@@ -28,7 +48,7 @@ class FoldersSettingsPage extends StatelessWidget {
           future: memesProvider.getAllFolders,
           builder: (ctx, AsyncSnapshot<List<Folder>> snapshot) =>
               snapshot.hasData
-                  ? _foldersListView(snapshot.data)
+                  ? _foldersListView(memesProvider, snapshot.data)
                   : Text('Wait...'),
         ),
       ),
