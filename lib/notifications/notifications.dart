03fe1abf26b5ac622c2960b56f3a4079a7bc3019
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-05-03 17:59:08 +0200

    Fix icon

--- lib/notifications/notifications.dart
+++ lib/notifications/notifications.dart
@@ -3,7 +3,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 class Notifications {
   static initializePlugin() {
     FlutterLocalNotificationsPlugin().initialize(InitializationSettings(
-      AndroidInitializationSettings('ic_launcher.png'),
+      AndroidInitializationSettings('ic_launcher'),
       IOSInitializationSettings(),
     ));
   }
