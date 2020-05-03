c457e9a6125f19efdf8fc870a37f002c20c03967
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-05-03 21:17:39 +0200

    Tested, i can remove that

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
 
     Future.microtask((){
       Notifications.initializePlugin();
-      Notifications.showExampleNotification();
     });
 
     return MultiProvider(
