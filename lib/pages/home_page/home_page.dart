770af9d31653a2a1ef49bd65fb01e671333c0d35
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-05-10 00:56:42 +0200

    Add file watchers

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -4,7 +4,9 @@ import 'package:dotmeme/pages/home_page/selection_app_bar.dart';
 import 'package:dotmeme/providers/home_page_provider.dart';
 import 'package:dotmeme/providers/memes_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 import 'package:provider/provider.dart';
+import 'package:watcher/watcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,6 +16,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // TODO: Clear textField focus when keyboard hidden by back press
 
+  // +++++ NOW +++++
+  // TODO: Add file listeners
+  // TODO: Sync when changing settings
+  // +++++ NOW +++++
+
   @override
   void initState() {
     super.initState();
@@ -21,6 +28,66 @@ class _HomePageState extends State<HomePage> {
       var homeProvider = Provider.of<HomePageProvider>(context, listen: false);
       var memesProvider = Provider.of<MemesProvider>(context, listen: false);
       homeProvider.memesList = await memesProvider.getAllMemes;
+
+      // Add file watchers
+      for (var folder in await memesProvider.getAllFolders) {
+        var assPath = await AssetPathEntity.fromId(folder.id.toString());
+        var singleAss =
+            (await assPath.getAssetListRange(start: 0, end: 1)).first;
+        if (singleAss == null) continue;
+        var file = await singleAss.file;
+
+        var newSyncGoing = false;
+        var newSyncScheduled = false;
+        var deleteSyncGoing = false;
+        var deleteSyncScheduled = false;
+
+        newSync() async {
+          if (newSyncGoing) {
+            newSyncScheduled = true;
+            return;
+          }
+          newSyncGoing = true;
+          print('NewSync running...');
+          await memesProvider.syncNewMemesInFolder(folder.id);
+          homeProvider.memesList = await memesProvider.getAllMemes;
+          newSyncGoing = false;
+          if (newSyncScheduled) {
+            newSyncScheduled = false;
+            newSync();
+          }
+        }
+
+        deleteSync() async {
+          if (deleteSyncGoing) {
+            deleteSyncScheduled = true;
+            return;
+          }
+          deleteSyncGoing = true;
+          print('DeleteSync running...');
+          await memesProvider.syncDeletedMemesInFolder(folder.id);
+          homeProvider.memesList = await memesProvider.getAllMemes;
+          deleteSyncGoing = false;
+          if (deleteSyncScheduled) {
+            deleteSyncScheduled = false;
+            deleteSync();
+          }
+        }
+
+        var watcher = DirectoryWatcher(file.parent.path);
+        watcher.events.listen((event) async {
+          print(event);
+          if (event.type == ChangeType.ADD) {
+            newSync();
+          } else if (event.type == ChangeType.REMOVE) {
+            deleteSync();
+          } else {
+            newSync();
+            deleteSync();
+          }
+        });
+      }
+
       await memesProvider.syncMemes();
       homeProvider.memesList = await memesProvider.getAllMemes;
     });
