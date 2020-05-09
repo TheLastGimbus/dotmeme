bc29a5c38155597831be715e1b9fc663aadc74a9
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-05-09 16:39:50 +0200

    Add delete option

--- lib/pages/home_page/selection_app_bar.dart
+++ lib/pages/home_page/selection_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:dotmeme/providers/home_page_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 import 'package:provider/provider.dart';
 
 Widget selectionAppBar(BuildContext context) {
@@ -14,7 +15,15 @@ Widget selectionAppBar(BuildContext context) {
     ),
     actions: <Widget>[
       IconButton(icon: Icon(Icons.share)),
-      IconButton(icon: Icon(Icons.delete)),
+      IconButton(
+        icon: Icon(Icons.delete),
+        onPressed: () {
+          var idsToDelete = homeProvider.selectControl.selection.selectedIndexes
+              .map((index) => homeProvider.memesList[index].id.toString())
+              .toList();
+          PhotoManager.editor.deleteWithIds(idsToDelete);
+        },
+      ),
       IconButton(icon: Icon(Icons.more_vert)),
     ],
     backgroundColor: Colors.black38,
