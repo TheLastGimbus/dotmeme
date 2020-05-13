fa083a3e37b5b5b278b221c13bd41ed26e879d11
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-05-14 01:19:20 +0200

    Add fullscreen mode

--- lib/pages/swiping_page/swiping_page.dart
+++ lib/pages/swiping_page/swiping_page.dart
@@ -5,6 +5,7 @@ import 'package:dotmeme/providers/home_page_provider.dart';
 import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:image/image.dart' as nImage;
 import 'package:path/path.dart' as path;
 import 'package:photo_manager/photo_manager.dart';
@@ -30,6 +31,8 @@ class SwipingPage extends StatefulWidget {
 }
 
 class _SwipingPageState extends State<SwipingPage> {
+  var fullscreen = false;
+
   Future<Uint8List> _loadMemeToMemory(Meme meme) async {
     var asset = await AssetEntity.fromId(meme.id.toString());
     var file = await asset.file;
@@ -79,46 +82,57 @@ class _SwipingPageState extends State<SwipingPage> {
     final homeProvider = Provider.of<HomePageProvider>(context);
     final _controller = PageController(initialPage: widget.startIndex);
 
+    SystemChrome.setEnabledSystemUIOverlays(
+        fullscreen ? [SystemUiOverlay.bottom] : SystemUiOverlay.values);
+
     // TODO: Show/hide app bar and options buttons (not present yet)
     //  on single press
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        leading: BackButton(),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.share),
-            onPressed: () async {
-              var asset = await AssetEntity.fromId(
-                homeProvider.memesList[_controller.page.round()].id.toString(),
-              );
-              var file = await asset.file;
-              var image = nImage.decodeNamedImage(
-                  await file.readAsBytes(), asset.title);
-              nImage.drawString(
-                image,
-                nImage.arial_24,
-                image.width ~/ 12,
-                image.height - image.height ~/ 8,
-                'Found with .meme',
-                color: Colors.white.value,
-              );
-              Share.file(
-                'Shere meme',
-                asset.title,
-                nImage.encodeJpg(image),
-                'image/${path.extension(asset.title).replaceFirst('.', '')}',
-              );
-            },
-          )
-        ],
-        backgroundColor: Colors.transparent,
-      ),
+      appBar: fullscreen
+          ? null
+          : AppBar(
+              leading: BackButton(),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.share),
+                  onPressed: () async {
+                    var asset = await AssetEntity.fromId(
+                      homeProvider.memesList[_controller.page.round()].id
+                          .toString(),
+                    );
+                    var file = await asset.file;
+                    var image = nImage.decodeNamedImage(
+                        await file.readAsBytes(), asset.title);
+                    nImage.drawString(
+                      image,
+                      nImage.arial_24,
+                      image.width ~/ 12,
+                      image.height - image.height ~/ 8,
+                      'Found with .meme',
+                      color: Colors.white.value,
+                    );
+                    Share.file(
+                      'Shere meme',
+                      asset.title,
+                      nImage.encodeJpg(image),
+                      'image/${path.extension(asset.title).replaceFirst('.', '')}',
+                    );
+                  },
+                )
+              ],
+              backgroundColor: Colors.transparent,
+            ),
       extendBodyBehindAppBar: true,
       body: PhotoViewGallery.builder(
         pageController: _controller,
         builder: (context, index) => PhotoViewGalleryPageOptions.customChild(
           child: _pageWidget(index, homeProvider.memesList[index]),
+          onTapUp: (ctx, details, value) {
+            setState(() {
+              fullscreen = !fullscreen;
+            });
+          },
           minScale: PhotoViewComputedScale.contained,
           maxScale: 50.0,
           scaleStateCycle: (scaleState) {
