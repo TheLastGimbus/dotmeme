3fbc464af90d022e921250137e12976f90121f41
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-07-02 22:47:51 +0200

    TDD :sunglasses:

--- test/basic_home_test.dart
+++ test/basic_home_test.dart
@@ -12,5 +12,12 @@ void main() {
 
     expect(find.byType(AppBar), findsOneWidget);
     expect(find.byIcon(Icons.settings), findsOneWidget);
+
+    // Load some memes
+    await tester.pumpAndSettle(const Duration(milliseconds: 1000));
+
+    // Some should be displayed
+    expect(find.byType(Image), findsWidgets);
+    expect(find.text("Loading"), findsNothing);
   });
 }
