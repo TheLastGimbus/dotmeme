6d520bb921f8265efa770bf1c25d012f14813fda
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-07-04 19:03:39 +0200

    Reformat media_sync_cubit.dart

--- lib/ui/common/cubit/media_sync_cubit.dart
+++ lib/ui/common/cubit/media_sync_cubit.dart
@@ -1,18 +1,25 @@
 import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 
 import '../../../database/media_sync.dart';
 import '../../../database/memebase.dart';
-import '../../../device_media/media_manager.dart';
 
+/// This cubit is big brain of syncing memes from device to db (while app open)
+///
+/// All events that require sync (HomePage opened etc) should be available
+/// as it's methods
+///
+/// However, we also need to do similar things when in background, so any
+/// independent logic of it should also be moved to different (non-cubit) place
+///
+/// TODO: File watchers
 class MediaSyncCubit extends Cubit<void> {
   final Memebase db;
-  final _mm = GetIt.I<MediaManager>();
 
-  MediaSyncCubit(this.db) : super(null) {}
+  MediaSyncCubit(this.db) : super(null);
 
+  /// Execute when user opens app
   void appOpenSync() async {
     final devFolders = await MediaSync.getMediaFolders();
     await db.enabledFoldersMemeSync(devFolders);
