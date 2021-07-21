f1812e7e124bc9a9aa1feb7c803827157b59ce38
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-07-21 11:26:43 +0200

    Fix curl redirect

--- scripts/setup-ocr.sh
+++ scripts/setup-ocr.sh
@@ -7,6 +7,6 @@ cd ..
 mkdir -p assets/tessdata/
 cd assets/tessdata/
 
-curl https://github.com/tesseract-ocr/tessdata_fast/raw/master/eng.traineddata --output eng.traineddata
+curl -L https://github.com/tesseract-ocr/tessdata_fast/raw/master/eng.traineddata > eng.traineddata
 # For unit tests
 sudo apt install tesseract-ocr
