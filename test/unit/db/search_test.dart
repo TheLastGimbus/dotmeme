3f1b7027fe66d5de2a49dd000f23e950817bf4da
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-08-21 01:01:32 +0200

    Move stuff to setUp and tearDown

--- test/unit/db/search_test.dart
+++ test/unit/db/search_test.dart
@@ -4,41 +4,44 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:moor/moor.dart';
 
 void main() {
-  const _memez = {
-    54352: 'this is text number one',
-    65436: 'this however is text number two',
-    90352: 'and this, unexpectedly, is text number three',
-    90675: 'oh this is four',
-    34503: '...and thats five...',
-  };
-  setupTestDb() async {
-    final db = Memebase(Memebase.virtualDatabase);
-    await db.batch((b) {
-      for (final entry in _memez.entries) {
-        b.insert(
-          db.memes,
-          Meme(
-            id: entry.key,
-            folderId: 69,
-            memeType: MemeType.image.index,
-            lastModified: DateTime.now(),
-            scannedText: entry.value,
-            textScannerVersion: -1,
-          ),
-        );
-      }
-    });
-    return db;
-  }
-
-  expectSearch(Memebase db, String string, Set<int> ids) async {
-    final result = await db.searchMemesByScannedText(string).get();
-    expect(result.map((e) => e.id).toSet(), ids);
-  }
-
   group("FTS5 search functionality", () {
+    const _memez = {
+      54352: 'this is text number one',
+      65436: 'this however is text number two',
+      90352: 'and this, unexpectedly, is text number three',
+      90675: 'oh this is four',
+      34503: '...and thats five...',
+    };
+    late Memebase db;
+    setUp(() async {
+      db = Memebase(Memebase.virtualDatabase);
+      await db.batch((b) {
+        for (final entry in _memez.entries) {
+          b.insert(
+            db.memes,
+            Meme(
+              id: entry.key,
+              folderId: 69,
+              memeType: MemeType.image.index,
+              lastModified: DateTime.now(),
+              scannedText: entry.value,
+              textScannerVersion: -1,
+            ),
+          );
+        }
+      });
+      return db;
+    });
+    tearDown(() async {
+      await db.close();
+    });
+
+    expectSearch(Memebase db, String string, Set<int> ids) async {
+      final result = await db.searchMemesByScannedText(string).get();
+      expect(result.map((e) => e.id).toSet(), ids);
+    }
+
     test("basic raw search", () async {
-      final db = await setupTestDb();
       await expectSearch(db, "this", {54352, 65436, 90352, 90675});
       await expectSearch(db, "and", {90352, 34503});
       await expectSearch(db, "number", {54352, 65436, 90352});
@@ -46,7 +49,6 @@ void main() {
           db, "thats OR is", {54352, 65436, 90352, 90675, 34503});
     });
     test("delete", () async {
-      final db = await setupTestDb();
       await expectSearch(db, "this", {54352, 65436, 90352, 90675});
       await (db.delete(db.memes)..where((e) => e.id.equals(65436))).go();
       await expectSearch(db, "this", {54352, 90352, 90675});
@@ -58,7 +60,6 @@ void main() {
       expect(res.length, 3);
     });
     test("update", () async {
-      final db = await setupTestDb();
       await expectSearch(db, "this", {54352, 65436, 90352, 90675});
       await (db.update(db.memes)..where((tbl) => tbl.id.equals(54352)))
           .write(const MemesCompanion(scannedText: Value("text number one")));
