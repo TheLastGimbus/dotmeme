54a9d7d7012cbd571c4d97e62d9fca75e78c5838
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-08-21 00:39:00 +0200

    Export functions

--- test/unit/db/search_test.dart
+++ test/unit/db/search_test.dart
@@ -10,35 +10,39 @@ void main() {
     90675: 'oh this is four',
     34503: '...and thats five...',
   };
-
-  group("Search functionality", () {
-    test("raw query", () async {
-      final db = Memebase(Memebase.virtualDatabase);
-      // Maybe move this to setup() ?
-      await db.batch((b) {
-        for (final entry in _memez.entries) {
-          b.insert(
-            db.memes,
-            Meme(
-              id: entry.key,
-              folderId: 69,
-              memeType: MemeType.image.index,
-              lastModified: DateTime.now(),
-              scannedText: entry.value,
-              textScannerVersion: -1,
-            ),
-          );
-        }
-      });
-      expectResults(String string, Set<int> ids) async {
-        final result = await db.searchMemesByScannedText(string).get();
-        expect(result.map((e) => e.id).toSet(), ids);
+  setupTestDb() async {
+    final db = Memebase(Memebase.virtualDatabase);
+    await db.batch((b) {
+      for (final entry in _memez.entries) {
+        b.insert(
+          db.memes,
+          Meme(
+            id: entry.key,
+            folderId: 69,
+            memeType: MemeType.image.index,
+            lastModified: DateTime.now(),
+            scannedText: entry.value,
+            textScannerVersion: -1,
+          ),
+        );
       }
+    });
+    return db;
+  }
 
-      await expectResults("this", {54352, 65436, 90352, 90675});
-      await expectResults("and", {90352, 34503});
-      await expectResults("number", {54352, 65436, 90352});
-      await expectResults("thats OR is", {54352, 65436, 90352, 90675, 34503});
+  expectSearch(Memebase db, String string, Set<int> ids) async {
+    final result = await db.searchMemesByScannedText(string).get();
+    expect(result.map((e) => e.id).toSet(), ids);
+  }
+
+  group("FTS5 search functionality", () {
+    test("basic raw search", () async {
+      final db = await setupTestDb();
+      await expectSearch(db, "this", {54352, 65436, 90352, 90675});
+      await expectSearch(db, "and", {90352, 34503});
+      await expectSearch(db, "number", {54352, 65436, 90352});
+      await expectSearch(
+          db, "thats OR is", {54352, 65436, 90352, 90675, 34503});
     });
   });
 }
