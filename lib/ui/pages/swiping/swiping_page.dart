a728da8d18296407d085ade1728708e6a819ca40
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-08-23 00:24:27 +0200

    Add share functionality

--- lib/ui/pages/swiping/swiping_page.dart
+++ lib/ui/pages/swiping/swiping_page.dart
@@ -2,16 +2,30 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../../database/memebase.dart';
 import '../../common/cubit/common_cache_cubit.dart';
 
-class SwipingPage extends StatelessWidget {
+class SwipingPage extends StatefulWidget {
   final List<Meme> memes;
   final int initialIndex;
 
   const SwipingPage({Key? key, required this.memes, this.initialIndex = 0})
       : super(key: key);
+
+  @override
+  _SwipingPageState createState() => _SwipingPageState();
+}
+
+class _SwipingPageState extends State<SwipingPage> {
+  late PageController pageCtrl;
+
+  @override
+  void initState() {
+    super.initState();
+    pageCtrl = PageController(initialPage: widget.initialIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +34,10 @@ class SwipingPage extends StatelessWidget {
       body: Stack(
         children: [
           PageView.builder(
-            controller: PageController(initialPage: initialIndex),
-            itemCount: memes.length,
+            controller: pageCtrl,
+            itemCount: widget.memes.length,
             itemBuilder: (context, index) => GestureDetector(
-              child: _image(context, memes[index]),
+              child: _image(context, widget.memes[index]),
               onVerticalDragEnd: (details) {
                 if (details.velocity.pixelsPerSecond.direction > 0) {
                   Navigator.of(context).pop();
@@ -31,13 +45,14 @@ class SwipingPage extends StatelessWidget {
               },
             ),
           ),
-          Align(alignment: Alignment.bottomCenter, child: _bottomBar()),
+          Align(alignment: Alignment.bottomCenter, child: _bottomBar(context)),
         ],
       ),
     );
   }
 
-  Widget _bottomBar() {
+  Widget _bottomBar(BuildContext context) {
+    final cacheCbt = context.read<CommonCacheCubit>();
     return Material(
       color: Colors.transparent,
       child: Container(
@@ -54,8 +69,15 @@ class SwipingPage extends StatelessWidget {
           children: [
             IconButton(
               icon: const Icon(Icons.share, color: Colors.white),
-              // TODO: Share
-              onPressed: () {},
+              onPressed: () async {
+                // TODO: Watermark
+                final currentMeme = widget.memes[pageCtrl.page!.round()];
+                final file = await cacheCbt.getFileWithCache(currentMeme.id);
+                if (file == null) {
+                  throw "Can't share meme $currentMeme because file is null!";
+                }
+                Share.shareFiles([file.path]);
+              },
             ),
           ],
         ),
