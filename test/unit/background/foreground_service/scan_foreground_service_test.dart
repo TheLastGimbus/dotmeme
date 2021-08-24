977c9fdb46f403ccca2f94d9db399eb27189af48
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-08-25 00:15:10 +0200

    Refactor scan_foreground_service_test.dart

--- test/unit/background/foreground_service/scan_foreground_service_test.dart
+++ test/unit/background/foreground_service/scan_foreground_service_test.dart
@@ -6,8 +6,26 @@ import 'package:dotmeme/database/queries.dart';
 import 'package:dotmeme/di.dart' as di;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 void main() {
+  late Memebase db;
+  late List<AssetPathEntity> deviceFolders;
+  // Sets up full database with memes synced and Reddit enabled
+  setUp(() async {
+    di.init(di.Environment.test);
+    db = GetIt.I<Memebase>();
+    deviceFolders = await MediaSync.getMediaFolders();
+    await db.foldersSync(deviceFolders);
+    await db.allFoldersMemeSync(deviceFolders);
+    await db.setFolderEnabled(
+      (await db.allFolders.get()).firstWhere((f) => f.name == "Reddit").id,
+      true,
+    );
+  });
+  tearDown(() async {
+    await GetIt.I.reset();
+  });
   group("ScanForegroundService", () {
     /// memeId <-> text that ocr is expected to spit out
     const _texts = {
@@ -15,41 +33,20 @@ void main() {
       1315402535634264:
           "Me leaving the apple\nstore with an Ban\n12 in my mouth:\n\na\n\n \n\f",
     };
+    late ForegroundServiceManager fsm;
+    setUp(() => fsm = GetIt.I<ForegroundServiceManager>());
+    tearDown(() async => await fsm.dispose());
 
     test("basic check if memes are being scanned", () async {
-      di.init(di.Environment.test);
-      // Init stuff and enable "Reddit"
-      final db = GetIt.I<Memebase>();
-      final deviceFolders = await MediaSync.getMediaFolders();
-      await db.foldersSync(deviceFolders);
-      await db.setFolderEnabled(
-        (await db.allFolders.get()).firstWhere((f) => f.name == "Reddit").id,
-        true,
-      );
-
-      final fsm = GetIt.I<ForegroundServiceManager>();
       await fsm.startScanService();
       await fsm.receiveStream.firstWhere((e) => e == null);
 
       for (final m in await db.allMemes.get()) {
         expect(m.scannedText, _texts[m.id]);
       }
-      await fsm.dispose();
-      await GetIt.I.reset();
     });
 
     test("don't re-scan memes if they have text already", () async {
-      di.init(di.Environment.test);
-      // Init stuff and enable "Reddit"
-      final db = GetIt.I<Memebase>();
-      final deviceFolders = await MediaSync.getMediaFolders();
-      await db.foldersSync(deviceFolders);
-      await db.allFoldersMemeSync(deviceFolders);
-      await db.setFolderEnabled(
-        (await db.allFolders.get()).firstWhere((f) => f.name == "Reddit").id,
-        true,
-      );
-
       // Set text ourselves before scan service does it
       for (final id in _texts.keys) {
         await db.setMemeScannedText(
@@ -59,7 +56,6 @@ void main() {
         );
       }
 
-      final fsm = GetIt.I<ForegroundServiceManager>();
       await fsm.startScanService();
       await fsm.receiveStream.firstWhere((e) => e == null);
 
@@ -67,8 +63,6 @@ void main() {
       for (final m in await db.allMemes.get()) {
         expect(m.scannedText, "Pre-set text here :)");
       }
-      await fsm.dispose();
-      await GetIt.I.reset();
     });
   });
 }
