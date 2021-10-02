932f3ba1a944ec59cba2385469d3a00371459887
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-10-02 22:32:21 +0200

    ...script-slavery too

--- scripts/setup-ocr.sh
+++ scripts/setup-ocr.sh
@@ -7,6 +7,6 @@ cd ..
 mkdir -p assets/tessdata/
 cd assets/tessdata/
 
-curl -L https://github.com/tesseract-ocr/tessdata_fast/raw/master/eng.traineddata > eng.traineddata
+curl -L https://github.com/tesseract-ocr/tessdata_fast/raw/main/eng.traineddata > eng.traineddata
 # For unit tests
 sudo apt install tesseract-ocr
